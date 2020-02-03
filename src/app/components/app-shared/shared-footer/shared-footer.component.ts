a0bc80f24722a57b81d4371d6aa6951bdcdbcfb5
junior1998 <juniorliban@hotmail.com>
2020-02-03 15:35:55 -0400

    Responsive y arreglos en el navbar

--- src/app/components/app-shared/shared-footer/shared-footer.component.ts
+++ src/app/components/app-shared/shared-footer/shared-footer.component.ts
@@ -6,11 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shared-footer.component.scss']
 })
 export class SharedFooterComponent implements OnInit {
+
+  // ====================================================================
+  // En la matriz de una dimencion que esta abajo
+  // es donde se agregan las redes sociales para que salgan en el footer
+  // ====================================================================
   
   redesSociales = [
     {
-      nommbre: 'facebook',
+      nommbre: 'Facebook',
       icono: 'fa-facebook-f',
+      enlace: 'https://www.facebook.com/DORHUGPS/'
+    },
+    {
+      nommbre: 'Instagram',
+      icono: 'fa-instagram',
       enlace: 'https://www.facebook.com/DORHUGPS/'
     }
   ];
